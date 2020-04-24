7d356cbd6e9c38197cecd20d9fd9c0b533655a7a
AdrianD <adrian950113@gmail.com>
2020-04-24 15:39:36 +0200

    submit on enter

--- src/AddNew/AddNew.tsx
+++ src/AddNew/AddNew.tsx
@@ -1,11 +1,12 @@
-import React, {useContext, useState} from 'react';
+import React, { useContext, useState, KeyboardEventHandler } from 'react';
 import './AddNew.css';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faSave, faUndo} from '@fortawesome/free-solid-svg-icons';
-import {useTheme} from '../hooks/useTheme';
-import {CategoryContext} from '../context/category.context';
-import {AppStateContext} from '../context/state.context';
-import {Firebase} from '../firebase';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSave, faUndo } from '@fortawesome/free-solid-svg-icons';
+import { useTheme } from '../hooks/useTheme';
+import { CategoryContext } from '../context/category.context';
+import { AppStateContext } from '../context/state.context';
+import { Firebase } from '../firebase';
+import { KeyObject } from 'crypto';
 
 export interface AddNewProps {
   onReturn: () => void;
@@ -18,29 +19,42 @@ export default function AddNew(props: AddNewProps) {
   const [text, setText] = useState<string>('');
   const rows = Math.floor(text.length / 10);
   const save = () => {
-    Firebase.getInstance().db.collection('categories').doc(category).set({
-      dishes: [...dishes, text]
-    });
+    Firebase.getInstance()
+      .db.collection('categories')
+      .doc(category)
+      .set({
+        dishes: [...dishes, text]
+      });
     props.onReturn();
+  };
+  const onEnter: KeyboardEventHandler = event => {
+    console.log(event);
+    if (event.key === 'Enter') {
+      save();
+    }
   };
   return (
     <div className="add-new-wrapper" style={theme.style}>
       <div className="input-wrapper">
-        <textarea className="category-name" placeholder="Podaj nazwę dania"
-                  rows={rows}
-                  value={text}
-                  onChange={event => setText(event.target.value)}/>
+        <textarea
+          className="category-name"
+          placeholder="Podaj nazwę dania"
+          rows={rows}
+          value={text}
+          onKeyUp={onEnter}
+          onChange={event => setText(event.target.value)}
+        />
       </div>
       <div style={theme.style} className="color-picker-tabs">
         <label className="color-picker-tab" onClick={() => save()}>
-          <FontAwesomeIcon icon={faSave} style={{paddingRight: '10px'}}/>
+          <FontAwesomeIcon icon={faSave} style={{ paddingRight: '10px' }} />
           Zapisz
         </label>
         <label className="color-picker-tab" onClick={props.onReturn}>
-          <FontAwesomeIcon icon={faUndo} style={{paddingRight: '10px'}}/>
+          <FontAwesomeIcon icon={faUndo} style={{ paddingRight: '10px' }} />
           Powrót
         </label>
       </div>
     </div>
-  )
+  );
 }
